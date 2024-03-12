aa79059e5d69800ef75c3f09409055bf1c8ac802
OpenWrt Builder <buster-openwrt@ovvo.uk>
2024-03-12 21:44:50 +0800

    qualcommax:ipq60xx: add Redmi AX5、Xiaomi AX1800

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -23,3 +23,29 @@ define Device/netgear_wax214
        DEVICE_PACKAGES := ipq-wifi-netgear_wax214
 endef
 TARGET_DEVICES += netgear_wax214
+
+define Device/redmi_ax5
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Redmi
+	DEVICE_MODEL := AX5
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax5
+endef
+TARGET_DEVICES += redmi_ax5
+
+define Device/xiaomi_ax1800
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Xiaomi
+	DEVICE_MODEL := AX1800
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax1800
+endef
+TARGET_DEVICES += xiaomi_ax1800
--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -10,6 +10,8 @@ platform_check_image() {
 
 platform_do_upgrade() {
 	case "$(board_name)" in
+	redmi,ax5|\
+	xiaomi,ax1800|\
 	netgear,wax214)
 		nand_do_upgrade "$1"
 		;;
