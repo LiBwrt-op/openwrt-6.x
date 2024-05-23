bf9870f735c848cad7a91f6bd1a84425e3e6844b
breeze303 <122870904+breeze303@users.noreply.github.com>
2024-05-23 21:10:48 +0800

    Update mac80211.sh set wireless=0

--- package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+++ package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
@@ -203,7 +203,7 @@ detect_mac80211() {
 			set wireless.${name}.channel=${channel}
 			set wireless.${name}.band=${mode_band}
 			set wireless.${name}.htmode=$htmode
-			set wireless.${name}.disabled=1
+			set wireless.${name}.disabled=0
 
 			set wireless.default_${name}=wifi-iface
 			set wireless.default_${name}.device=${name}
