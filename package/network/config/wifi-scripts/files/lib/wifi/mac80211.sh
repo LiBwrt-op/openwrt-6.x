de18950bd23d246cc6ce0ac32f01c36dd02fd25c
breeze303 <grapenskrskr@gmail.com>
2024-06-11 14:31:07 +0800

    Add Statement

--- package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+++ package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
@@ -209,7 +209,7 @@ detect_mac80211() {
 			set wireless.default_${name}.device=${name}
 			set wireless.default_${name}.network=lan
 			set wireless.default_${name}.mode=ap
-			set wireless.default_${name}.ssid=OpenWrt
+			set wireless.default_${name}.ssid=LibWrt
 			set wireless.default_${name}.encryption=none
 EOF
 		uci -q commit wireless
