c6a928cafbf392459d67e380ffc9a3a7fd05154d
breeze303 <grapenskrskr@gmail.com>
2024-12-19 09:59:06 +0800

    ipq60xx: update mk

--- target/linux/qualcommax/ipq60xx/target.mk
+++ target/linux/qualcommax/ipq60xx/target.mk
@@ -1,9 +1,10 @@
 SUBTARGET:=ipq60xx
 BOARDNAME:=Qualcomm Atheros IPQ60xx
 DEFAULT_PACKAGES += \
-	ath11k-firmware-ipq6018 nss-firmware-ipq6018 kmod-qca-nss-dp kmod-ath11k-ahb \
-	kmod-qca-nss-drv kmod-qca-nss-ecm \
-	kmod-qca-nss-drv-bridge-mgr
+	ath11k-firmware-ipq6018 kmod-ath11k-ahb \
+	kmod-qca-nss-dp kmod-qca-nss-drv kmod-qca-nss-drv-bridge-mgr \
+	kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-vlan-mgr kmod-qca-nss-drv-lag-mgr \
+	kmod-qca-nss-ecm kmod-qca-mcs kmod-qca-ssdk nss-firmware-ipq6018
 
 define Target/Description
 	Build firmware images for Qualcomm Atheros IPQ60xx based boards.
